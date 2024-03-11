d922946df69eb21e4a8f3cc2739f837ee2c83e55
MAHESH2056 <maheschy2323@gmaill.com>
2024-03-11 22:10:36 +0545

    MVC Pattern Extensions Packages

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
-const express = require('express');
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+const cors = require("cors");
+
+//dot config
+dotenv.config();
 
 //rest object
 const app = express();
 
+//middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
 //routes
-//1 test routes
-app.get('/',(req,res) => {
-    res.status(200).json({
-        message: "Welcome to Blood Bank App",
-    });
-});
+// 1 test route
+app.use("/api/v1/test", require("./routes/testRoutes"));
 
 //port
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 //listen
 app.listen(PORT, () => {
-    console.log("Node Server Running");
+  console.log(
+    `Node Server Running In ${process.env.DEV_MODE} ModeOn Port ${process.env.PORT}`
+      .bgBlue.white
+  );
 });
