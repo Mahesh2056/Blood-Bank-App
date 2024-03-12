f2e52cc17e02a055aba8fe79c50c8ab7eeca0840
MAHESH2056 <maheschy2323@gmaill.com>
2024-03-12 21:55:17 +0545

    Register API with Postman Tool

---
+++ routes/authRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { registerController } = require("../controllers/authcontroller");
+
+const router = express.Router();
+
+//routes
+router.post("/register", registerController);
+
+module.exports = router;
