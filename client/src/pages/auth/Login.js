898f9e2c07cabaff00f1295d4b14d103ddc1eee0
MAHESH2056 <maheschy2323@gmaill.com>
2024-03-16 21:51:14 +0545

    create Reusable Login Register Page

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Form from '../../components/shared/Form/Form';
 
 const Login = () => {
   return (
-    <div>
-        <h1>Login Page</h1>
-    </div>
+    <>
+      <div className="row g-0">
+        <div className="col-md-8 form-banner">
+            <img src="./assets/images/banner1.jpg" alt="loginImage" />
+        </div>
+        <div className="col-md-4 form-container">
+          <Form 
+            formTitle={"Login"} 
+            submitBtn={"Login"} 
+            formType={"login"} 
+          />
+        </div>
+      </div>
+    </>
   )
 }
 
