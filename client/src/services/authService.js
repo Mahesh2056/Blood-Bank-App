2c8afd3c11fe1726e007a11646953e98704a1e77
MAHESH2056 <maheschy2323@gmaill.com>
2024-03-17 22:24:45 +0545

    Login Action and Reducer

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,17 +1,20 @@
+import { userLogin } from "../redux/features/auth/authActions";
+import store from "../redux/store";
+
 export const handleLogin = (e,email,password,role) => {
     e.preventDefault();
     try {
        if (!role || !email || !password) {
         return alert("Please Privde All Feilds");
        }
-       console.log("login", e, email, password, role);
+       store.dispatch(userLogin({ email, password, role }));
     } catch (error) {
        console.log(error); 
     }
 };
 
 export const handleRegister = (
-    e,
+  e,
   name,
   role,
   email,
