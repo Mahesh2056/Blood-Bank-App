9c2971d5daed392220ca44e859b8730010025e92
MAHESH2056 <maheschy2323@gmaill.com>
2024-03-20 21:49:30 +0545

    Create Layout And Logut action

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import Spinner from "../components/shared/Spinner";
+import Layout from "../components/shared/Layout/Layout";
 
 const HomePage = () => {
   const { loading, error } = useSelector((state) => state.auth);
   return (
-    <>
+    <Layout>
       {error && <span>{alert(error)}</span>}
       {loading ? (
         <Spinner/>
@@ -14,7 +15,7 @@ const HomePage = () => {
           <h1>Home Page</h1>
         </>
       )}
-    </>
+    </Layout>
   );
 };
 
