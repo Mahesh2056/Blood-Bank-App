0bc67c47aec347550578acbe30a1a146f6c79582
MAHESH2056 <maheschy2323@gmaill.com>
2024-03-26 23:11:03 +0545

    Project Completed Admin Panel operations

--- client/src/pages/Dashboard/Consumer.js
+++ client/src/pages/Dashboard/Consumer.js
@@ -1,49 +1,49 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../../components/shared/Layout/Layout";
-import API from "../../services/API";
 import moment from "moment";
+import API from "../../services/API";
 import { useSelector } from "react-redux";
 
 const Consumer = () => {
   const { user } = useSelector((state) => state.auth);
-  const[data, setData] = useState([]);
+  const [data, setData] = useState([]);
   //find donar records
   const getDonars = async () => {
-      try {
-        const { data } = await API.post("/inventory/get-inventory-hospital", {
-          filters: {
-            inventoryType: "out",
-            hospital: user?._id,
-          },
-        });
-        if (data?.success) {
-         setData(data?.inventory);
-         console.log(data);
-        }
-      } catch (error) {
-          console.log(error);
+    try {
+      const { data } = await API.post("/inventory/get-inventory-hospital", {
+        filters: {
+          inventoryType: "out",
+          hospital: user?._id,
+        },
+      });
+      if (data?.success) {
+        setData(data?.inventory);
+        console.log(data);
       }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
-      getDonars();
+    getDonars();
   }, []);
 
-return (
-  <Layout>
-    <div className="container mt-4">
-      <table className="table">
+  return (
+    <Layout>
+      <div className="container mt-4">
+        <table className="table">
           <thead>
-           <tr>
-            <th scope="col">Blood Group</th>
-            <th scope="col">Inventory Type</th>
-            <th scope="col">Quantity</th>
-            <th scope="col">Email</th>
-            <th scope="col">Date</th>
-           </tr>
+            <tr>
+              <th scope="col">Blood Group</th>
+              <th scope="col">Inventory TYpe</th>
+              <th scope="col">Quantity</th>
+              <th scope="col">Email</th>
+              <th scope="col">Date</th>
+            </tr>
           </thead>
           <tbody>
-            {data?.map((record) =>(
+            {data?.map((record) => (
               <tr key={record._id}>
                 <td>{record.bloodGroup}</td>
                 <td>{record.inventoryType}</td>
@@ -54,8 +54,8 @@ return (
             ))}
           </tbody>
         </table>
-    </div>
-  </Layout>
+      </div>
+    </Layout>
   );
 };
 
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const colors = require("colors");
 const morgan = require("morgan");
 const cors = require("cors");
 const connectDB = require("./config/db");
-
 //dot config
 dotenv.config();
 
@@ -25,6 +24,7 @@ app.use("/api/v1/test", require("./routes/testRoutes"));
 app.use("/api/v1/auth", require("./routes/authRoutes"));
 app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
 app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
 
 //port
 const PORT = process.env.PORT || 8080;
